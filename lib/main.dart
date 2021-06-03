617d294865718f6d43424edc57074b0a4177b200
SSG\sangnd <sangnd@ssg.vn>
2021-06-03 12:35:48 +0700

    IAmRich-DinhSang: hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+// Them main function is the starting point for all our Flutter app
 void main() {
   runApp(
     MaterialApp(
-      home: Text('Hello world'),
+      home: Center(
+        child: Text('Hello world'),
+      ),
     ),
   );
 }
