20a5c168e8201ac615051c7f873305664cc68713
SSG\sangnd <sangnd@ssg.vn>
2021-06-04 14:49:16 +0700

    IAmRich-DinhSang: Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
       ),
     ),
   );
