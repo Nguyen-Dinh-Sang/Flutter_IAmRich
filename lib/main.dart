2f7973d3e0d7cbb846596db8ec846a02dc51d450
SSG\sangnd <sangnd@ssg.vn>
2021-06-07 11:25:37 +0700

    IAmRich-DinhSang: scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,16 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://img2.thuthuatphanmem.vn/uploads/2019/01/04/hinh-anh-hot-girl-dep_025104603.jpg'),
+          ),
         ),
       ),
     ),
